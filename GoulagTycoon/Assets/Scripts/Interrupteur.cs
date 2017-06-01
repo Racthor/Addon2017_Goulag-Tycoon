275e5c40de96ed511eebc22d83bfadb840d131e2
Rousselot Mathis <mathisr2207@hotmail.fr>
2017-06-01 03:14:16 +0200

    Interrupteur + "Grille"

---
+++ GoulagTycoon/Assets/Scripts/Interrupteur.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interrupteur : MonoBehaviour
+{
+    public Vector3 posx;
+    public GameObject player_able;
+    public GameObject hunter;
+    public GameObject Zone;
+    public GameObject Grille;
+
+    private bool used = false;
+    private bool accept = false;
+    private int nbJS;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    nbJS = player_able.GetComponent<Player>().nbJS;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    
+    void OnTriggerStay(Collider element)
+    {
+        Debug.Log("nbJs " + nbJS + " " + Input.GetButtonDown("Joystick 1 Button A") + " " + Input.GetButtonDown("Joystick 2 Button A") + " " + Input.GetButtonDown("Joystick 3 Button A"));
+        if (element.gameObject == player_able && used == false && Input.GetButtonDown("Joystick " + nbJS + " Button A"))
+        {
+            Debug.Log("ooo");
+            used = true;
+            hunter.transform.position = Grille.transform.position;
+
+        }
+    }
+}
